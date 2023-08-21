c891bea52e387700590cdac9b3d0e86e419947a6
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-08-21 00:38:30 -0300

    Initial commit

---
+++ src/tp/pkg5/laboratorio/Producto.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.pkg5.laboratorio;
+
+/**
+ *
+ * @author Netbook
+ */
+public class Producto {
+    private String categoria;
+    private String nombre;
+    private float precio ; 
+
+    public Producto() {
+    }
+
+    public Producto(String categoria, String nombre, float precio) {
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
